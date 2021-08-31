37da34e36c93f85fb90d7c7a2252fe81e720924e
GameDesert <56128805+GameDesert@users.noreply.github.com>
2021-08-31 22:50:07 +0100

    stuff

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ A Python module to help decode IATA-standard boarding passes with barcodes.
 
 Data is usually stored on PDF417, QR Code, Aztec, or Data Matrix barcodes as defined by IATA Resolution 792.
 
+[Module on PyPi](https://pypi.org/project/bcbp/)
 
 Read more about the standard:
 
@@ -26,13 +27,31 @@ You will need the `datetime`, `re`, and `json` modules installed for BCBP to fun
 The following commands are available in BCBP:
 
 - `bcbp.decode("[IATA Resolution 792-standard BCBP data string.]")`
+- `bcbp.encode("[firstname]", "[lastname]", "[pnr]", "[origin]", "[destination]", "[operator]", "[flight number]", "[date]", "[cabin class]", "[seat number]", "[check-in sequence]", [softerror])`
+
+Parameters for the `bcbp.encode()` command:
+
+| Parameter | Data Type | Values | Description | 
+| ----------- | ----------- | ----------- | ----------- |
+| Firstname | String | Any value which, when combined with Lastname, does not exceed 19 characters. | The passenger's first name, can include titles "MR","MRS","MSTR", "DR", or "MS" after the name. |
+| Lastname | String | Any value which, when combined with Firstname, does not exceed 19 characters. | The passenger's last name. |
+| PNR | String | Any value which does not exceed 7 characters. | The Passenger Name Reference (also referred to as a Booking Reference). |
+| Origin | String | 3-character IATA airport code. | [Search Airport Codes Here (IATA Website)](https://www.iata.org/en/publications/directories/code-search/) |
+| Destination | String | 3-character IATA airport code. | [Search Airport Codes Here (IATA Website)](https://www.iata.org/en/publications/directories/code-search/) |
+| Operator | String | 2-character airline/operator code. | [Search Airline Codes Here (IATA Website)](https://www.iata.org/en/publications/directories/code-search/) |
+| Flight Number | String | Any value which does not exceed 4 characters. | The flight number, usually preceeded by the operator code but this value must only be the number. |
+| Date | String | A Python datetime date object with at least a month and a day. | A month and day which can be translated into the julian date. Must be a Python [datetime](https://docs.python.org/3/library/datetime.html) date. |
+| Cabin Class | String | 1-character value. | A single character symbol that represents the class the seat is located in. |
+| Seat Number | String | 4-character value. | The passenger's seat, usually a 3-digit row followed by a single character seat identifier. |
+| Check-In Sequence | String | 4-character value. | A 4-digit number that shows which check-in the passenger had (e.g: 1st passenger to check in: 0001, 25th passenger to check in: 0025). |
+| Softerror | Boolean | A boolean (True/False) value. | Set to `True` for the command to return an error message in the output rather than a BCBP code. Set to `false` for an exception to be raised whenever there is an error. |
 
 ---
 
 ## Planned Upgrades
 
-- Error catching when improper string is passed.
-- `bcbp.encode()` function.
+- ~~Error catching when improper string is passed.~~
+- ~~`bcbp.encode()` function.~~
 
 ---
 
--- bcbp/bcbp.py
+++ bcbp/bcbp.py
@@ -9,21 +9,101 @@ def decode(bcbp):
 
     leg = bcbp[1]
 
-    name_bcbp = bcbp[2:22].replace(" ", "")
-    fullname = name_bcbp.split("/")
-    result["firstname"] = str(re.sub('(MRS|MR|MS|MSTR|DR)$','',fullname[1]).replace(" ", ""))
-    result["lastname"] = str(fullname[0].replace(" ", ""))
+    try:
+        name_bcbp = bcbp[2:22].replace(" ", "")
+        fullname = name_bcbp.split("/")
+        result["firstname"] = str(re.sub('(MRS|MR|MS|MSTR|DR)$','',fullname[1]).replace(" ", ""))
+        result["lastname"] = str(fullname[0].replace(" ", ""))
 
-    result["pnr"] = str(bcbp[22:30].replace(" ", ""))
-    result["origin"] = str(bcbp[30:33].replace(" ", ""))
-    result["destination"] = str(bcbp[33:36].replace(" ", ""))
-    result["operator"] = str(bcbp[36:38].replace(" ", ""))
-    result["flight_number"] = str(bcbp[39:43].replace(" ", ""))
-    result["date"] = str(datetime.datetime.strptime(bcbp[44:47], '%j').strftime('%d/%b'))
-    result["cabin_class"] = str(bcbp[47].replace(" ", ""))
-    result["seat_number"] = str(bcbp[48:52].replace(" ", ""))
-    result["checkin_sequence"] = str(bcbp[52:56].replace(" ", ""))
+        result["pnr"] = str(bcbp[22:30].replace(" ", ""))
+        result["origin"] = str(bcbp[30:33].replace(" ", ""))
+        result["destination"] = str(bcbp[33:36].replace(" ", ""))
+        result["operator"] = str(bcbp[36:38].replace(" ", ""))
+        result["flight_number"] = str(bcbp[39:43].replace(" ", ""))
+        result["date"] = str(datetime.datetime.strptime(bcbp[44:47], '%j').strftime('%d/%b'))
+        result["cabin_class"] = str(bcbp[47].replace(" ", ""))
+        result["seat_number"] = str(bcbp[48:52].replace(" ", ""))
+        result["checkin_sequence"] = str(bcbp[52:56].replace(" ", ""))
+        output = json.dumps(result)
 
-    output = json.dumps(result)
+        return output
+    except:
+        return '{"error":"unable to parse string - incorrect format"}'
 
-    return output
+def encode(firstname, lastname, pnr, origin, destination, operator, flight_number, date, cabin_class, seat_number, checkin_sequence, softerror=False):
+    fullname_pre = (lastname + "/" + firstname).upper().replace(" ","").replace("-","")
+    if len(fullname_pre) < 20:
+        fullname = fullname_pre + (" " * (20 - int(len(fullname_pre))))
+    else:
+        if softerror == True:
+            return "Error: Set softerror to False to see full error."
+        else:
+            raise ValueError('BCBP Encode Error','Fullname > 20 Chars')
+    
+    pnr = pnr.upper()
+    if len(pnr) <= 7:
+        pnr = pnr + (" " * (7 - int(len(pnr))))
+    else:
+        if softerror == True:
+            return "Error: Set softerror to False to see full error."
+        else:
+            raise ValueError('BCBP Encode Error','PNR > 7 Chars')
+
+
+    odo_info = (origin + destination + operator).replace(" ","").upper()
+    if len(odo_info) <= 8:
+        odo = odo_info + (" " * (8 - int(len(odo_info))))
+    else:
+        if softerror == True:
+            return "Error: Set softerror to False to see full error."
+        else:
+            raise ValueError('BCBP Encode Error','Origin and destination must be 3 characters long. Operator must be 2 characters long.')
+    
+    flight_no = (flight_number).replace(" ","").upper()
+    if len(flight_no) <= 4:
+        flight_no = flight_no + (" " * (4 - int(len(flight_no))))
+    else:
+        if softerror == True:
+            return "Error: Set softerror to False to see full error."
+        else:
+            raise ValueError('BCBP Encode Error','Flight Number > 4 Chars')
+
+    julian_date = (date.strftime('%j')).replace(" ","").upper()
+    if len(str(julian_date)) <= 3:
+        julian_date = julian_date + (" " * (3 - int(len(julian_date))))
+    else:
+        if softerror == True:
+            return "Error: Set softerror to False to see full error."
+        else:
+            raise ValueError('BCBP Encode Error','Date > 3 Chars')
+
+    cabin = (cabin_class).replace(" ","").upper()
+    if len(cabin) <= 1:
+        cabin = cabin + (" " * (1 - int(len(cabin))))
+    else:
+        if softerror == True:
+            return "Error: Set softerror to False to see full error."
+        else:
+            raise ValueError('BCBP Encode Error','Cabin Class > 1 Chars')
+
+    seat = (seat_number).replace(" ","").upper()
+    if len(seat) <= 4:
+        seat = seat + (" " * (4 - int(len(seat))))
+    else:
+        if softerror == True:
+            return "Error: Set softerror to False to see full error."
+        else:
+            raise ValueError('BCBP Encode Error','Seat Number > 4 Chars')
+    
+    checkin = (checkin_sequence).replace(" ","").upper()
+    if len(checkin) <= 4:
+        checkin = checkin + (" " * (4 - int(len(checkin))))
+    else:
+        if softerror == True:
+            return "Error: Set softerror to False to see full error."
+        else:
+            raise ValueError('BCBP Encode Error','Check-In Sequence > 4 Chars')
+
+    bcbp = "M1{}{} {} {} {}{}{}{}".format(fullname, pnr, odo, flight_no, julian_date, cabin, seat, checkin).upper()
+
+    return bcbp
